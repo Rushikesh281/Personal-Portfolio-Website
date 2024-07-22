b373c2fff0f7c14102c2b96d0c501bf936dbdfcf
Rushikesh281 <rushikeshsapkal2526@gmail.com>
2024-07-22 17:35:10 +0530

    first commit

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+    experience: [
+      {
+        title: "Consultant",
+        subTitle: "Miami, FL",
+        description:
+          " User Experience, Visual Design",
+      },
+      {
+        title: "Senior Consultant",
+        subTitle: "London, UK",
+        description:
+          "Web development, Databases, Backend developement",
+      },
+      {
+        title: "Project Manager",
+        subTitle: "Bangalore, INDIA",
+        description:
+          "Creative Direction Project Management, Team Leading",
+      },
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "Creative Public School",
+        description:
+          "10th Grade",
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "Sri Chaitanya Junior College",
+        description:
+          "12th Grade in Science",
+      },
+      {
+        title: "B.E",
+        subTitle: "Don Bosco institute of technolgy",
+        description:
+          "Bachelor of technology in computer science",
+      },
+    ],
+  };
